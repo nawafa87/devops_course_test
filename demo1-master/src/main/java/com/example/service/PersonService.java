6242cec976dce23893219fd88f916567051fd80c
nawafa87 <63101603+nawafa87@users.noreply.github.com>
2022-07-08 22:49:58 +0300

    Update PersonService.java

--- demo1-master/src/main/java/com/example/service/PersonService.java
+++ demo1-master/src/main/java/com/example/service/PersonService.java
@@ -40,6 +40,13 @@ public class PersonService {
 		p.setLastName("Mahmoud");
 		persons.add(p);
 		
+		p = new Person();
+		p.setId("5");
+		p.setAge(23);
+		p.setFirstName("Nawaf");
+		p.setLastName("Abdulaziz");
+		persons.add(p);
+		
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
